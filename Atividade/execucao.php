0ac717da61f37577d29c0e9dd1731b269ba0b530
JonathanBeckerBCK <jonathanbe.ifpr2024@gmail.com>
2026-03-26 15:11:08 -0300

    Add files via upload

---
+++ Atividade/execucao.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = $_GET["nome"];
+$sobrenome = $_GET["sobrenome"];
+$idade = $_GET["idade"];
+$profissao = $_GET["profissao"];
+
+print "<h1>Meu nome é ".$nome." ".$sobrenome.", e tenho ".$idade." anos e sou ".$profissao.".</h1>";
+
